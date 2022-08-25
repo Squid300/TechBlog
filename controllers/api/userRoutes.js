e1e845431cbbc541558a40091e53457ff457063e
Squid300 <squid400@gmail.com>
2022-08-24 23:42:45 -0400

    added routes and logic for login and account creation

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,67 @@
 const router = require('express').Router();
-const {} = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
-router.get( '/', async ( req, res ) => {
+router.post( '/login', async ( req, res ) => {
+    try {
+        const response = req;
+        const userData = await User.findOne({ where: { email: req.body.email }});
 
+        if(!userData){
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword){
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        }
+
+        req.session.save(() =>{
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/create', async ( req, res ) => {
+    try{
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+
+            res.status(200).json(userData)
+        });
+    }catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.get('/profile', async ( req, res ) => {
+    try{
+        const userData = await User.findByPk( req.session.user_id, {
+            attributes: [
+                'username',
+                'email',
+            ]
+        });
+        const postData = await Post.findAll({ where: { user_id: req.session.user_id }});
+        const commentData = await Comment.findAll({ where: { user_id: req.session.user_id }});
+
+        res.render('profile', {
+            userData,
+            postData,
+            commentData,
+            loggedIn: req.session.loggedIn,
+        });
+    }catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 module.exports = router;
